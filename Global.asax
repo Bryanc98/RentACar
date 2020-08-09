1d6040a1158a7ebc95c24c315102d0c4d04f70f6
Bryanc98 <48458983+Bryanc98@users.noreply.github.com>
2020-08-08 20:54:34 -0400

    Creating the project

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RentACar.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentACar.Startup))]
+namespace RentACar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
